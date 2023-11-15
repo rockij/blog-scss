9ffc8185588224a6f01a356dbd45ba4ef7e9b50a
rockij <rockij83@nate.com>
2023-11-15 23:11:48 +0900

    231115

---
+++ resources/js/items.js
@@ -0,0 +1,22 @@
+let items = [
+  {
+    productName: "switch - gooey toggle",
+    category: 'button',
+    url: 'html/switch-gooey-toggle.html',
+  },
+  {
+    productName: 'button - 아쿠아 느낌의 마우스오버시 바운스',
+    category: 'button',
+    url: 'html/button-aqua-hover-bounce.html',
+  },
+  {
+    productName: 'button - 애니메이션 줄무늬 그라데이션',
+    category: 'button',
+    url: 'html/button-animatted-striped-gradient.html',
+  },
+  // {
+  //   productName: '',
+  //   category: '',
+  //   url: 'html/.html',
+  // },
+]
